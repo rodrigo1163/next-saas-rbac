e188e8a8653e99905821d49703d214afc75b51ad
rodrigo1163 <rodrigo.sena1163@gmail.com>
2025-08-25 17:55:57 -0400

    feat: adicionar endpoint para obter informações de associação do usuário em uma organização

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -16,6 +16,7 @@ import { billingSubject } from './subjects/billing'
 export * from './models/organization'
 export * from './models/project'
 export * from './models/user'
+export * from './roles'
 
 const appAbilitiesSchema = z.union([
   projectSubject,
