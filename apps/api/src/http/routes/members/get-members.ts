f328ac01f5fadd1bc9c225171a7d8b81b4b2647f
rodrigo1163 <rodrigo.sena1163@gmail.com>
2025-08-27 16:49:12 -0400

    feat: registrar endpoint para obter membros

--- apps/api/src/http/routes/members/get-members.ts
+++ apps/api/src/http/routes/members/get-members.ts
@@ -1,3 +1,4 @@
+import { roleSchema } from '@saas/auth'
 import type { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod/v4'
@@ -6,84 +7,84 @@ import { auth } from '@/http/middlewares/auth'
 import { prisma } from '@/lib/prisma'
 import { getUserPermissions } from '@/utils/get-user-permissions'
 
-import { BadRequestError } from '../_errors/bad-request-error'
 import { UnauthorizedError } from '../_errors/unauthorized-error'
 
-export async function getProject(app: FastifyInstance) {
+export async function getMembers(app: FastifyInstance) {
   app
     .withTypeProvider<ZodTypeProvider>()
     .register(auth)
     .get(
-      '/organizations/:orgSlug/projects/:projectSlug',
+      '/organizations/:slug/members',
       {
         schema: {
-          tags: ['projects'],
-          summary: 'Get a project',
+          tags: ['members'],
+          summary: 'Get all members',
           security: [{ bearerAuth: [] }],
           params: z.object({
-            orgSlug: z.string(),
-            projectSlug: z.string(),
+            slug: z.string(),
           }),
           response: {
             200: z.object({
-              project: z.object({
-                id: z.uuid(),
-                description: z.string(),
-                name: z.string(),
-                slug: z.string(),
-                avatarUrl: z.url().nullable(),
-                organizationId: z.uuid(),
-                ownerId: z.uuid(),
-                owner: z.object({
+              members: z.array(
+                z.object({
+                  userId: z.uuid(),
                   id: z.uuid(),
+                  role: roleSchema,
                   name: z.string().nullable(),
+                  email: z.email(),
                   avatarUrl: z.url().nullable(),
                 }),
-              }),
+              ),
             }),
           },
         },
       },
       async (request, reply) => {
-        const { orgSlug, projectSlug } = request.params
+        const { slug } = request.params
         const userId = await request.getCurrentUserId()
         const { organization, membership } =
-          await request.getUserMembership(orgSlug)
+          await request.getUserMembership(slug)
 
         const { cannot } = getUserPermissions(userId, membership.role)
 
-        if (cannot('get', 'Project')) {
-          throw new UnauthorizedError(`You're not allowed to see this project.`)
+        if (cannot('get', 'User')) {
+          throw new UnauthorizedError(
+            `You're not allowed to see organization members.`,
+          )
         }
 
-        const project = await prisma.project.findUnique({
+        const members = await prisma.member.findMany({
           select: {
             id: true,
-            name: true,
-            description: true,
-            slug: true,
-            ownerId: true,
-            avatarUrl: true,
-            organizationId: true,
-            owner: {
+            role: true,
+            user: {
               select: {
                 id: true,
                 name: true,
+                email: true,
                 avatarUrl: true,
               },
             },
           },
           where: {
-            slug: projectSlug,
             organizationId: organization.id,
+          },
+          orderBy: {
+            role: 'asc',
           },
         })
 
-        if (!project) {
-          throw new BadRequestError('Project not found')
-        }
+        const membersWithRoles = members.map(
+          ({ user: { id: userId, ...user }, ...member }) => {
+            return {
+              ...user,
+              ...member,
+              userId,
+            }
+          },
+        )
 
-        return reply.status(200).send({ project })
+        return reply.status(200).send({ members: membersWithRoles })
       },
     )
 }
