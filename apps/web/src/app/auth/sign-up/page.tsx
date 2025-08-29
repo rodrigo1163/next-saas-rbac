b2b4175c0c712a6069c28994600c8d92066f72a9
rodrigo1163 <rodrigo.sena1163@gmail.com>
2025-08-29 18:27:44 -0400

    feat: add sign-up page

--- apps/web/src/app/auth/sign-up/page.tsx
+++ apps/web/src/app/auth/sign-up/page.tsx
@@ -7,38 +7,43 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Separator } from '@/components/ui/separator'
 
-export default function SignInPage() {
+export default function SignUpPage() {
   return (
     <form action="" className="space-y-4">
+      <div className="space-y-1">
+        <Label htmlFor="name">Name</Label>
+        <Input name="name" id="name" />
+      </div>
       <div className="space-y-1">
         <Label htmlFor="email">E-mail</Label>
         <Input name="email" type="email" id="email" />
       </div>
       <div className="space-y-1">
-        <Label htmlFor="password">Senha</Label>
+        <Label htmlFor="password">Password</Label>
         <Input name="password" type="password" id="password" />
-
-        <Link
-          href="/auth/forgot-password"
-          className="text-foreground text-xs font-medium hover:underline"
-        >
-          Forgot your password?
-        </Link>
+      </div>
+      <div className="space-y-1">
+        <Label htmlFor="password_confirmation">Confirm your passwords</Label>
+        <Input
+          name="password_confirmation"
+          type="password"
+          id="password_confirmation"
+        />
       </div>
 
       <Button type="submit" className="w-full">
-        Sign in with e-mail
+        Create account
       </Button>
 
       <Button variant="link" className="w-full" size="sm" asChild>
-        <Link href="/auth/sign-up">Create new account</Link>
+        <Link href="/auth/sign-in">Already registered? Sign in</Link>
       </Button>
 
       <Separator />
 
       <Button type="submit" variant="outline" className="w-full">
         <Image src={githubIcon} className="mr-2 size-4 dark:invert" alt="" />
-        Sign in with Github
+        Sign up with Github
       </Button>
     </form>
   )
