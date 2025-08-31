02b23bc23f6a56e06d8750d5466f24450ded6640
rodrigo1163 <rodrigo.sena1163@gmail.com>
2025-08-30 22:07:10 -0400

    feat: implementar autenticação de usuário e adicionar suporte a cookies com a biblioteca cookies-next

--- apps/web/src/auth/auth.ts
+++ apps/web/src/auth/auth.ts
@@ -1,7 +1,28 @@
 import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+import { getProfile } from '@/http/get-profile'
 
 export async function isAuthenticated() {
   const cookiesStore = await cookies()
 
   return !!cookiesStore.get('token-rbac')?.value
+}
+
+export async function auth() {
+  const cookiesStore = await cookies()
+
+  const token = cookiesStore.get('token-rbac')?.value
+
+  if (!token) {
+    redirect('/auth/sign-in')
+  }
+
+  try {
+    const { user } = await getProfile()
+
+    return { user }
+  } catch (error) {}
+
+  redirect('/api/auth/sign-out')
 }
--- apps/web/src/http/api-client.ts
+++ apps/web/src/http/api-client.ts
@@ -1,5 +1,29 @@
+import { getCookie as getCookieClient } from 'cookies-next'
+import {
+  type CookiesFn,
+  getCookie as getCookieServer,
+} from 'cookies-next/server'
 import ky from 'ky'
 
 export const api = ky.create({
   prefixUrl: 'http://localhost:3333',
+  hooks: {
+    beforeRequest: [
+      async (request) => {
+        let cookieStore: CookiesFn | undefined
+        let token = getCookieClient('token-rbac')
+
+        if (typeof window === 'undefined') {
+          const { cookies: serverCookies } = await import('next/headers')
+
+          cookieStore = serverCookies
+          token = await getCookieServer('token-rbac', { cookies: cookieStore })
+        }
+
+        if (token) {
+          request.headers.set('Authorization', `Bearer ${token}`)
+        }
+      },
+    ],
+  },
 })
