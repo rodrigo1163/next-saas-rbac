1f0d2bbf638082b0571ab1b968a69768c184d8fa
rodrigo1163 <rodrigo.sena1163@gmail.com>
2025-08-31 12:09:01 -0400

    feat: adicionar lógica de autenticação e gerenciamento de permissões no cabeçalho e na seleção de organização

--- apps/web/src/auth/auth.ts
+++ apps/web/src/auth/auth.ts
@@ -1,12 +1,48 @@
+import { defineAbilityFor } from '@saas/auth'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
+import { getMembership } from '@/http/get-membership'
 import { getProfile } from '@/http/get-profile'
 
 export async function isAuthenticated() {
   const cookiesStore = await cookies()
 
   return !!cookiesStore.get('token-rbac')?.value
+}
+
+export async function getCurrentOrg() {
+  const cookiesStore = await cookies()
+  const currentOrg = cookiesStore.get('org')?.value ?? null
+
+  return currentOrg
+}
+
+export async function getCurrentMemberShip() {
+  const org = await getCurrentOrg()
+
+  if (!org) {
+    return null
+  }
+
+  const { membership } = await getMembership(org)
+
+  return membership
+}
+
+export async function ability() {
+  const membership = await getCurrentMemberShip()
+
+  if (!membership) {
+    return null
+  }
+
+  const ability = defineAbilityFor({
+    id: membership.userId,
+    role: membership.role,
+  })
+
+  return ability
 }
 
 export async function auth() {
